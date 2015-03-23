73c581af339251748627acbf47274e34c833e232
BoBeR182 <BoBeR182@users.noreply.github.com>
2015-03-23 05:16:17 -0400

    update 2.0 for securesms name change

--- src/org/smssecure/smssecure/Release.java
+++ src/org/smssecure/smssecure/Release.java
@@ -2,6 +2,6 @@ package org.smssecure.smssecure;
 
 public class Release {
 
-  public static final String PUSH_URL = "https://textsecure-service.securesms.org";
+  public static final String PUSH_URL = "https://securesms.org";//is this useless???
 //  public static final String PUSH_URL = "http://192.168.1.135:8080";
 }
