6bd009e46e8bc335b4eb229305c58dade63190af
William Desportes <williamdes@wdes.fr>
2018-03-01 14:13:17 +0100

    Replace for i with foreach

--- src/org/smssecure/smssecure/contacts/RecipientsEditor.java
+++ src/org/smssecure/smssecure/contacts/RecipientsEditor.java
@@ -280,9 +280,9 @@ public class RecipientsEditor extends AppCompatMultiAutoCompleteTextView {
     }
 
     private static String getAnnotation(Annotation[] a, String key) {
-        for (int i = 0; i < a.length; i++) {
-            if (a[i].getKey().equals(key)) {
-                return a[i].getValue();
+        for (Annotation anA : a) {
+            if (anA.getKey().equals(key)) {
+                return anA.getValue();
             }
         }
 
