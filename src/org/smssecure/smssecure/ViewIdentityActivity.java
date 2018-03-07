typ-AhmedSleem/Silence-Android

improve multi-SIM support
* Each SIM card is now bound to a unique key pair.
* Display number or slot index if display name is the same for multiple
  cards.
* Use subcription ID of last received message as default SIM, but do not
  change it if user is in conversation.
* Update app subscription IDs on new phone
* Start secure session only for SIM which received message, allow user to
  select SIM when starting a secure session

Fixes #467
Fixes #497

src/org/smssecure/smssecure/ViewIdentityActivity.java
@@ -19,11 +19,17 @@ package org.smssecure.smssecure;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import org.smssecure.smssecure.crypto.MasterSecret;
-import org.smssecure.smssecure.util.Hex;
-import org.whispersystems.libaxolotl.IdentityKey;
 import org.smssecure.smssecure.crypto.IdentityKeyParcelable;
+import org.smssecure.smssecure.crypto.IdentityKeyUtil;
+import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.util.dualsim.SubscriptionManagerCompat;
+import org.smssecure.smssecure.util.Hex;
+
+import org.whispersystems.libaxolotl.IdentityKey;
 
 /**
  * Activity for displaying an identity key.
@@ -39,11 +45,39 @@ public class ViewIdentityActivity extends KeyScanningActivity {
   private IdentityKey identityKey;
 
   @Override
-  protected void onCreate(Bundle state, @NonNull MasterSecret masterSecret) {
+  protected void onCreate(Bundle icicle, @NonNull MasterSecret masterSecret) {
+    int subscriptionId = getIntent().getIntExtra("subscription_id", SubscriptionManagerCompat.getDefaultMessagingSubscriptionId().or(-1));
+
+    getIntent().putExtra(ViewIdentityActivity.IDENTITY_KEY,
+                         new IdentityKeyParcelable(IdentityKeyUtil.getIdentityKey(this, subscriptionId)));
+    getIntent().putExtra(ViewIdentityActivity.TITLE,
+                         getString(R.string.ViewIdentityActivity_your_identity_fingerprint));
+
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     setContentView(R.layout.view_identity_activity);
 
     initialize();
+  }
+
+  @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    super.onPrepareOptionsMenu(menu);
+
+    MenuInflater inflater = this.getMenuInflater();
+    inflater.inflate(R.menu.local_identity, menu);
+
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    super.onOptionsItemSelected(item);
+
+    switch (item.getItemId()) {
+        case android.R.id.home:finish(); return true;
+    }
+
+    return false;
   }
 
   protected void initialize() {

src/org/smssecure/smssecure/jobs/MmsDownloadJob.java
@@ -118,7 +118,7 @@ public class MmsDownloadJob extends MasterSecretJob {
       }
 
       if (retrieveConf.getSubject() != null && WirePrefix.isEncryptedMmsSubject(retrieveConf.getSubject().getString())) {
-        MmsCipher    mmsCipher    = new MmsCipher(new SilenceAxolotlStore(context, masterSecret));
+        MmsCipher    mmsCipher    = new MmsCipher(new SilenceAxolotlStore(context, masterSecret, notification.get().second));
         RetrieveConf plaintextPdu = (RetrieveConf) mmsCipher.decrypt(context, retrieveConf);
 
         storeRetrievedMms(masterSecret, contentLocation, messageId, threadId, plaintextPdu, true, notification.get().second);

src/org/smssecure/smssecure/jobs/MmsSendJob.java
@@ -85,7 +85,7 @@ public class MmsSendJob extends SendJob {
 
       if (message.isSecure()) {
         Log.w(TAG, "Encrypting MMS...");
-        pdu = getEncryptedMessage(masterSecret, pdu);
+        pdu = getEncryptedMessage(masterSecret, pdu, message.getSubscriptionId());
         upgradedSecure = true;
       }
 
@@ -153,11 +153,11 @@ public class MmsSendJob extends SendJob {
     }
   }
 
-  private SendReq getEncryptedMessage(MasterSecret masterSecret, SendReq pdu)
+  private SendReq getEncryptedMessage(MasterSecret masterSecret, SendReq pdu, int subscriptionId)
       throws InsecureFallbackApprovalException, UndeliverableMessageException
   {
     try {
-      MmsCipher cipher = new MmsCipher(new SilenceAxolotlStore(context, masterSecret));
+      MmsCipher cipher = new MmsCipher(new SilenceAxolotlStore(context, masterSecret, subscriptionId));
       return cipher.encrypt(context, pdu);
     } catch (NoSessionException e) {
       throw new InsecureFallbackApprovalException(e);

BastienLQ/SecuredText

src/org/smssecure/smssecure/ViewIdentityActivity.java
@@ -19,11 +19,17 @@ package org.smssecure.smssecure;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import org.smssecure.smssecure.crypto.MasterSecret;
-import org.smssecure.smssecure.util.Hex;
-import org.whispersystems.libsignal.IdentityKey;
 import org.smssecure.smssecure.crypto.IdentityKeyParcelable;
+import org.smssecure.smssecure.crypto.IdentityKeyUtil;
+import org.smssecure.smssecure.crypto.MasterSecret;
+import org.smssecure.smssecure.util.dualsim.SubscriptionManagerCompat;
+import org.smssecure.smssecure.util.Hex;
+
+import org.whispersystems.libsignal.IdentityKey;
 
 /**
  * Activity for displaying an identity key.
@@ -39,11 +45,39 @@ public class ViewIdentityActivity extends KeyScanningActivity {
   private IdentityKey identityKey;
 
   @Override
-  protected void onCreate(Bundle state, @NonNull MasterSecret masterSecret) {
+  protected void onCreate(Bundle icicle, @NonNull MasterSecret masterSecret) {
+    int subscriptionId = getIntent().getIntExtra("subscription_id", SubscriptionManagerCompat.getDefaultMessagingSubscriptionId().or(-1));
+
+    getIntent().putExtra(ViewIdentityActivity.IDENTITY_KEY,
+                         new IdentityKeyParcelable(IdentityKeyUtil.getIdentityKey(this, subscriptionId)));
+    getIntent().putExtra(ViewIdentityActivity.TITLE,
+                         getString(R.string.ViewIdentityActivity_your_identity_fingerprint));
+
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     setContentView(R.layout.view_identity_activity);
 
     initialize();
+  }
+
+  @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    super.onPrepareOptionsMenu(menu);
+
+    MenuInflater inflater = this.getMenuInflater();
+    inflater.inflate(R.menu.local_identity, menu);
+
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    super.onOptionsItemSelected(item);
+
+    switch (item.getItemId()) {
+        case android.R.id.home:finish(); return true;
+    }
+
+    return false;
   }
 
   protected void initialize() {

src/org/smssecure/smssecure/jobs/MmsDownloadJob.java
@@ -119,7 +119,7 @@ public class MmsDownloadJob extends MasterSecretJob {
       }
 
       if (retrieveConf.getSubject() != null && WirePrefix.isEncryptedMmsSubject(retrieveConf.getSubject().getString())) {
-        MmsCipher    mmsCipher    = new MmsCipher(new SilenceSignalProtocolStore(context, masterSecret));
+        MmsCipher    mmsCipher    = new MmsCipher(new SilenceSignalProtocolStore(context, masterSecret, notification.get().second));
         RetrieveConf plaintextPdu = (RetrieveConf) mmsCipher.decrypt(context, retrieveConf);
 
         storeRetrievedMms(masterSecret, contentLocation, messageId, threadId, plaintextPdu, true, notification.get().second);

src/org/smssecure/smssecure/jobs/MmsSendJob.java
@@ -84,7 +84,7 @@ public class MmsSendJob extends SendJob {
 
       if (message.isSecure()) {
         Log.w(TAG, "Encrypting MMS...");
-        pdu = getEncryptedMessage(masterSecret, pdu);
+        pdu = getEncryptedMessage(masterSecret, pdu, message.getSubscriptionId());
         upgradedSecure = true;
       }
 
@@ -148,11 +148,11 @@ public class MmsSendJob extends SendJob {
     }
   }
 
-  private SendReq getEncryptedMessage(MasterSecret masterSecret, SendReq pdu)
+  private SendReq getEncryptedMessage(MasterSecret masterSecret, SendReq pdu, int subscriptionId)
       throws UndeliverableMessageException
   {
     try {
-      MmsCipher cipher = new MmsCipher(new SilenceSignalProtocolStore(context, masterSecret));
+      MmsCipher cipher = new MmsCipher(new SilenceSignalProtocolStore(context, masterSecret, subscriptionId));
       return cipher.encrypt(context, pdu);
     } catch (NoSessionException e) {
       throw new UndeliverableMessageException(e);